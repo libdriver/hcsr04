e48fc700476192469591a0203a6876594811244f
lishifeng <lishifenging@outlook.com>
2025-04-17 23:06:58 +0800

    fix: fix doc errors

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,10 +1,10 @@
 /**
  * @mainpage  LibDriver HCSR04
- * @brief     LibDriver HCSR04 is a full function driver of HCSR04 and it can run in both MCU and Linux platform.
+ * @brief     LibDriver HCSR04 is a full-featured driver of HCSR04 and it can run in both MCU and Linux platform.
  * @version   2.0.0
  * @author    Shifeng Li
  *
- * <h3>history</h3>
+ * <h3>History</h3>
  * <table>
  * <tr><th>Date        <th>Version  <th>Author      <th>Description
  * <tr><td>2021/04/15  <td>2.0      <td>Shifeng Li  <td>format the code
